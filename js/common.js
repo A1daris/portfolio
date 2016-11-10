83af52ee6688d5e41285b5dfdc750e99ba95a462
A1daris <sizov.boris@gmail.com>
2016-11-10 09:45:22 +0300

    contacts section added

--- js/common.js
+++ js/common.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 	//Pop up window (magnificPopup)
 	$(".popup").magnificPopup({type:"image"});
+	//pop up at portfolio section
+	$(".popup_content").magnificPopup({type:'inline', midClick: true, closeOnContentClick: true});
 
 	//Headers animation (animate.css)
 	$(".top_text h1").animated("fadeInDown", "fadeOutUp");
@@ -13,9 +15,19 @@ $(document).ready(function() {
 	//section work animation
 	$(".left .resume_item").animated("fadeInUp", "fadeOutUp");
 	$(".right .resume_item").animated("fadeInDown", "fadeOutDown");
-	
 
-	//Screen height detection
+
+	//tabs portfolio (mixitup plugin)
+	$("#portfolio_grid").mixItUp();
+	//toggle active tab  in portfolio section
+	$(".s_portfolio li").click(function() {
+		$(".s_portfolio li").removeClass("active");
+		$(this).addClass("active");
+	});
+
+
+
+	//first Screen height detection
 	function heightDetect() {
 		$(".main_head").css("height", $(window).height());
 	};
@@ -45,6 +57,16 @@ $(document).ready(function() {
 			$(".top_mnu li a").addClass("fadeInUp animated");
 		};
 	});
+	//set id's to portfolio items
+	$(".portfolio_item").each(function(i) {
+		$(this).find("a").attr("href", "#work_" + i);
+		$(this).find(".port_descr").attr("id", "work_" + i);
+	});
+	//form validation
+	$("input, select, textarea").jqBootstrapValidation();
+	
+	//page scroll to id plugin
+	$(".top_mnu ul a").mPageScroll2id();
 
 });
 
