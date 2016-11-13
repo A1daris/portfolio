cb8fc55645b86e090a9bf3ce9c8b2dc6063f9b56
A1daris <sizov.boris@gmail.com>
2016-11-13 13:02:55 +0300

    php files added

---
+++ index.php
@@ -0,0 +1,291 @@
+<?php get_header();?>
+
+<section id="about" class="s_about bg_light" >
+	<div class="section_header">
+		<h2><?php echo get_cat_name(2) ?></h2>
+		<div class="s_descr_wrap">
+			<div class="s_descr"><?php echo category_description(2) ?></div>
+		</div>
+	</div>
+	<div class="section_content">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 col-md-push-4 animation_1">
+					<h3>Фото</h3>
+					<div class="person">
+						<a href="img/photo.jpg" class="popup"><img src="img/photo.jpg" alt="alt"></a>
+					</div>
+				</div>
+				<div class="col-md-4 col-md-pull-4 animation_2">
+					<h3>
+Немного о себе
+</h3>
+					<p>Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Обеспечивает своего он парадигматическая продолжил взобравшись маленький подпоясал то за снова предупреждал последний всеми, пояс власти текстами предупредила вдали всемогущая.</p>
+					<p>Обеспечивает своего он парадигматическая продолжил взобравшись маленький подпоясал то за снова предупреждал последний всеми, пояс власти текстами предупредила вдали всемогущая.</p>
+				</div>
+				<div class="col-md-4 animation_3 personal_last_block">
+					<h3>Personal info</h3>
+					<h2 class="personal_header">Boris Sizov</h2>
+					<ul>
+						<li>Proffesional site making: scetches, design development, word press</li>
+						<li>Birthday: 10 january 1986</li>
+						<li>Phone: +7 904 679-9493</li>
+						<li>E-mail: <a href="mailto: boris.sizov@gmail.com">boris.sizof@gmail.com</a></li>
+					</ul>
+					<div class="social_wrap">
+						<ul>
+							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fa fa-vk"></i></a></li>
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-github"></i></a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-4"></div>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="resume" class="s_resume" >
+	<div class="section_header">
+		<h2>Resume</h2>
+		<div class="s_descr_wrap">
+			<div class="s_descr">My skills and achivements</div>
+		</div>
+	</div>
+	<div class="section_content">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 resume_container left">
+					<h3>Work</h3>
+					<div class="resume_icon">
+						<i class="icon-basic-book-pen"></i>
+					</div>
+					<div class="resume_item">
+						<div class="year">2010-2011</div>
+						<div class="resume_description">OOO Trollolo <strong>Programmer</strong>
+							<p>Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Предупреждал заманивший заглавных, языком щеке силуэт точках запятых толку семь своего живет, сих буквенных. Это послушавшись по всей свое вскоре океана?</p>
+						</div>
+					</div>
+					<div class="resume_item">
+						<div class="year">2010-2011</div>
+						<div class="resume_description">OOO Trollolo <strong>Programmer</strong>
+							<p>Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Предупреждал заманивший заглавных, языком щеке силуэт точках запятых толку семь своего живет, сих буквенных. Это послушавшись по всей свое вскоре океана?</p>
+						</div>
+					</div>
+					<div class="resume_item">
+						<div class="year">2010-2011</div>
+						<div class="resume_description">OOO Trollolo <strong>Programmer</strong>
+							<p>Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Предупреждал заманивший заглавных, языком щеке силуэт точках запятых толку семь своего живет, сих буквенных. Это послушавшись по всей свое вскоре океана?</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6 resume_container right">
+					<h3>Study</h3>
+					<div class="resume_icon">
+						<i class="icon-basic-case"></i>
+					</div>
+					<div class="resume_item">
+						<div class="year">2010-2011</div>
+						<div class="resume_description">OOO Trollolo <strong>Programmer</strong>
+							<p>Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Предупреждал заманивший заглавных, языком щеке силуэт точках запятых толку семь своего живет, сих буквенных. Это послушавшись по всей свое вскоре океана?</p>
+						</div>
+					</div>
+					<div class="resume_item">
+						<div class="year">2010-2011</div>
+						<div class="resume_description">OOO Trollolo <strong>Programmer</strong>
+							<p>Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Предупреждал заманивший заглавных, языком щеке силуэт точках запятых толку семь своего живет, сих буквенных. Это послушавшись по всей свое вскоре океана?</p>
+						</div>
+					</div>
+					<div class="resume_item">
+						<div class="year">2010-2011</div>
+						<div class="resume_description">OOO Trollolo <strong>Programmer</strong>
+							<p>Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Предупреждал заманивший заглавных, языком щеке силуэт точках запятых толку семь своего живет, сих буквенных. Это послушавшись по всей свое вскоре океана?</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="portfolio" class="s_portfolio bg_dark" >
+	<div class="section_header">
+		<h2>My works</h2>
+		<div class="s_descr_wrap">
+			<div class="s_descr">Web sites, I made recently </div>
+		</div>
+	</div>
+	<div class="section_content">
+		<div class="container">
+			<div class="row">
+				<div class="filter_div controls">
+					<ul>
+						<li class="filter active" data-filter="all">All project</li>
+						<li class="filter" data-filter=".category-1">Web sites</li>
+						<li class="filter" data-filter=".category-2">Logos</li>
+						<li class="filter" data-filter=".category-3">UI</li>
+					</ul>
+				</div>
+				<div id="portfolio_grid">
+					<div class="mix col-md-3 col-sm-6 col-xs-6 portfolio_item category-1">
+						<img src="img/portfolio-images/1.jpg" alt="">
+						<div class="port_item_cont">
+							<h3>Car interior</h3>
+							<p>Made in futurustic style, that design could lead us to the nice expirience of super fast driving</p>
+							<a class="popup_content">Fast view</a>
+							<div class="hidden">
+								<div class="port_descr">
+									<h3>Car interior</h3>
+									<p>Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Первую продолжил единственное текстов, до ведущими его большой взобравшись, родного большого домах путь семь скатился рукопись путь решила журчит за.</p>
+									<img src="img/portfolio-images/1.jpg" alt="Alt">
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="mix col-md-3 col-sm-6 col-xs-6 portfolio_item category-1"><img src="img/portfolio-images/2.jpg" alt="">
+						<div class="port_item_cont">
+							<h3>Robot drawing</h3>
+							<p>Made in futurustic style, that design could</p>
+							<a class="popup_content">Fast view</a>
+						</div>
+						<div class="hidden">
+								<div class="port_descr">
+									<h3>Car interior</h3>
+									<p>Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Первую продолжил единственное текстов, до ведущими его большой взобравшись, родного большого домах путь семь скатился рукопись путь решила журчит за.</p>
+									<img src="img/portfolio-images/2.jpg" alt="Alt">
+								</div>
+							</div>
+					</div>
+					<div class="mix col-md-3 col-sm-6 col-xs-6 portfolio_item category-1"><img src="img/portfolio-images/3.jpg" alt="">
+						<div class="port_item_cont">
+							<h3>Robot drawing</h3>
+							<p>Made in futurustic style, that design could</p>
+							<a class="popup_content">Fast view</a>
+						</div>
+						<div class="hidden">
+								<div class="port_descr">
+									<h3>Car interior</h3>
+									<p>Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Первую продолжил единственное текстов, до ведущими его большой взобравшись, родного большого домах путь семь скатился рукопись путь решила журчит за.</p>
+									<img src="img/portfolio-images/3.jpg" alt="Alt">
+								</div>
+							</div>
+					</div>
+					<div class="mix col-md-3 col-sm-6 col-xs-6 portfolio_item category-2"><img src="img/portfolio-images/4.jpg" alt="">
+						<div class="port_item_cont">
+							<h3>Robot drawing</h3>
+							<p>Made in futurustic style, that design could</p>
+							<a>Fast view</a>
+						</div>
+					</div>
+					<div class="mix col-md-3 col-sm-6 col-xs-6 portfolio_item category-2"><img src="img/portfolio-images/5.jpg" alt="">
+						<div class="port_item_cont">
+							<h3>Robot drawing</h3>
+							<p>Made in fauturustic style, that design could</p>
+							<a class="popup_content">Fast view</a>
+						</div>
+						<div class="hidden">
+								<div class="port_descr">
+									<h3>Car interior</h3>
+									<p>Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Первую продолжил единственное текстов, до ведущими его большой взобравшись, родного большого домах путь семь скатился рукопись путь решила журчит за.</p>
+									<img src="img/portfolio-images/4.jpg" alt="Alt">
+								</div>
+							</div>
+					</div>
+					<div class="mix col-md-3 col-sm-6 col-xs-6 portfolio_item category-3"><img src="img/portfolio-images/6.jpg" alt="">	<div class="port_item_cont">
+							<h3>Robot drawing</h3>
+							<p>Made in futurustic style, that design could</p>
+							<a class="popup_content">Fast view</a>
+						</div>
+						<div class="hidden">
+								<div class="port_descr">
+									<h3>Car interior</h3>
+									<p>Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Первую продолжил единственное текстов, до ведущими его большой взобравшись, родного большого домах путь семь скатился рукопись путь решила журчит за.</p>
+									<img src="img/portfolio-images/5.jpg" alt="Alt">
+								</div>
+							</div>
+					</div>
+					<div class="mix col-md-3 col-sm-6 col-xs-6 portfolio_item category-3"><img src="img/portfolio-images/StockSnap_AWASHBGWIL.jpg" alt="">
+						<div class="port_item_cont">
+							<h3>Gallery</h3>
+							<p>Made in futurustic style, that design could</p>
+							<a class="popup_content">Fast view</a>
+						</div>
+						<div class="hidden">
+								<div class="port_descr">
+									<h3>Car interior</h3>
+									<p>Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Первую продолжил единственное текстов, до ведущими его большой взобравшись, родного большого домах путь семь скатился рукопись путь решила журчит за.</p>
+									<img src="img/portfolio-images/StockSnap_AWASHBGWIL.jpg" alt="Alt">
+								</div>
+							</div>
+					</div>
+					<div class="mix col-md-3 col-sm-6 col-xs-6 portfolio_item category-3"><img src="img/portfolio-images/StockSnap_KWS03HELK6.jpg" alt="">
+						<div class="port_item_cont">
+							<h3>Flower</h3>
+							<p>Made in futurustic style, that design could</p>
+							<a class="popup_content">Fast view</a>
+						</div>
+						<div class="hidden">
+								<div class="port_descr">
+									<h3>Car interior</h3>
+									<p>Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Первую продолжил единственное текстов, до ведущими его большой взобравшись, родного большого домах путь семь скатился рукопись путь решила журчит за.</p>
+									<img src="img/portfolio-images/StockSnap_KWS03HELK6.jpg" alt="Alt">
+								</div>
+							</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="contacts" class="s_contacts bg_light" >
+	<div class="section_header">
+		<h2>Contact me</h2>
+		<div class="s_descr_wrap">
+			<div class="s_descr">Put your message below</div>
+		</div>
+	</div>
+	<div class="section_content">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="contact_box">
+						<div class="contacts_icon icon-basic-geolocalize-05"></div>
+						<h3>Adress:</h3>
+						<p>Lorem ipsum dolor sit amet</p>
+					</div>
+					<div class="contact_box">
+						<div class="contacts_icon icon-basic-webpage-img-txt"></div>
+						<h3>Phone</h3>
+						<p><a href="#">+7 812 232 42 12</a></p>
+					</div>
+					<div class="contact_box">
+						<div class="contacts_icon icon-basic-webpage-img-txt"></div>
+						<h3>Web site</h3>
+						<p><a href="#">www.bsizoff.cc</a></p>
+					</div>
+				</div>
+				<div class="col-md-6 col-sm-6">
+					<form action="" class="main_form" novalidate>
+						<label class="form_group">
+							<span class="color_element">*</span> Your name:
+							<input type="text" name="name" placeholder="Your name here" data-validation-required-message="ZZZZZ mothrfcr" required />
+							<span class="help-block text-danger">Please fill this form</span>
+						</label>
+						<label class="form_group">
+							<span class="color_element">*</span> E-mail:
+							<input type="email" name="email" placeholder="Your name here" data-validation-email-message="email is incorrect" required />
+							<span class="help-block text-danger">Please fill email</span>
+						</label>
+						<label class="form_group">
+							<span class="color_element">*</span> E-mail:
+							<textarea name="message" id="" cols="10" rows="4" placeholder="Your message here" data-validation-required-message="ZZZZZ mothrfcr" required></textarea>
+							<span class="help-block text-danger">Please fill email</span>
+						</label>
+						<button>Send</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<?php get_footer();?>
